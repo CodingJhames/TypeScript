9f69197cef159dc7c62ff9df4755d952b19115da
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-11-18 14:54:15 -0500

    public & private methods

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -2,20 +2,25 @@
 (()=> {
 
     class Avenger {
-        /* private name: string;
-        private team: string;
-        public realName?: string; */
         static avgAge: number = 35; 
+        static getAvgAge(){
+            return this.name;
+        }
 
         constructor( 
             private name: string,
             private team:string,
             public realName?:string,
             ){}
+
+            public bio(){
+                return `${this.name} (${this.team})`
+            }
     }
 
     const antman: Avenger = new Avenger('Antman', 'Captain', 'Scott lang');
-    console.log( antman );
+    console.log( antman.bio() );
 
     console.log( Avenger.avgAge );
+    console.log( Avenger.getAvgAge() );
 })()
--- bases/main.js
+++ bases/main.js
@@ -6,10 +6,17 @@
             this.team = team;
             this.realName = realName;
         }
+        static getAvgAge() {
+            return this.name;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'Captain', 'Scott lang');
-    console.log(antman);
+    console.log(antman.bio());
     console.log(Avenger.avgAge);
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
