603120a7a41c6c7f359f39f1fa87944728f08b35
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-05-01 08:31:51 -0500

    type inference

--- webtutorial/app.ts
+++ webtutorial/app.ts
@@ -6,3 +6,19 @@ let heading = document.createElement('h1');
 heading.textContent = message;
 
 document.body.appendChild( heading );
+
+// inference
+
+let counter: number = 0;
+
+function setCounter(  max:100) {
+    
+}
+
+function increment(counter:number): number {
+    return counter++;
+}
+
+document.addEventListener('click', function(event){
+    console.log( event.button );
+} )
