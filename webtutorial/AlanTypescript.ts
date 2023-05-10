9946e51ad4a6ef69d18b18f300628c34a6a4bd2b
codingjhames <jhames.mejia@unipamplona.edu.co>
2023-05-10 16:47:29 -0500

    Interaces added

--- webtutorial/AlanTypescript.ts
+++ webtutorial/AlanTypescript.ts
@@ -43,11 +43,61 @@ interface Persona {
     getVelocidad: ( param:number ) => number
 }
 
-const person = {
+const person : Persona = {
     nombre: 'Alan',
-    edad: 28,
+    edad: 28, 
     getVelocidad: (param) => {
         return param * 2;
     }
 }
+
+let personatwo = {
+    nombre: 'Maia',
+    edad: 34,
+    getVelocidad: ( param ) => {
+        return param * 2;
+    },
+};
+
+personatwo = person;
+
+const arrayCharacters: Character[] = [
+    {
+        id: 1,
+        name: 'james',
+        status: 'alive',
+        species: 'hooman',
+        type: 'Nikka',
+        gender: 'male',
+        origin: {
+            name: 'Earth',
+            url: 'www.jamesmejia.me',
+        },
+    },
+]
+
+arrayCharacters.forEach( c => c.origin.url );
+
+interface Character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: {
+    name: string;
+    url: string;
+    };
+};
+
+interface CharacterOrigin {
+    name: string;
+    url: string;
+};
+
+
+
+
+
 
